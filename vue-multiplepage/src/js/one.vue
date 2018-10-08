ad12ff4ab61c8d646ebc678ffe6def19c738284e
huangjiaqian <18775134221@163.com>
2018-10-08 19:20:26 +0800

    初始化

---
+++ vue-multiplepage/src/js/one.vue
@@ -0,0 +1,24 @@
+<template>
+  <a href="two.html" class="test" v-text="test"></a>
+</template>
+
+<script>
+export default {
+  name: 'one',
+  data () {
+    return {
+      test: ''
+    }
+  },
+  created () {
+    this.test = localStorage.getItem('name')
+  },
+  mounted () {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-multiplepage/src/js/two.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="test">two</div>
+</template>
+
+<script>
+export default {
+  name: 'two'
+}
+</script>
+
+<style scoped>
+
+</style>
